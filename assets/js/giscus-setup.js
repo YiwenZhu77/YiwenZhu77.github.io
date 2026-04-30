8d2f7c3fa3734647fabe2a06b0f6a93829b1706a
YiwenZhu77 <YiwenZhu77@users.noreply.github.com>
2026-04-30 17:14:37 +0000

    Deploying to gh-pages from @ YiwenZhu77/YiwenZhu77.github.io@2e4beec1cb6dd35beb6fd23406ee25a32111bcfa 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "YiwenZhu77/YiwenZhu77.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
